ce2e96ece9541df78d44e34d3db5ffdd99499680
Krsto Proroković <krstopro@krstos-mbp.home>
2018-05-07 10:46:21 +0200

    Second commit

--- manual.py
+++ manual.py
@@ -10,7 +10,7 @@ class LookupTableManual(nn.Module):
 		super(LookupTableManual, self).__init__()
 		self.embedding_size = embedding_size
 		self.embedding = nn.Embedding(vocab_size, embedding_size)
-		# self.linear = nn.Linear(?, output_size)
+		self.linear = nn.Linear(1, output_size)
 	
 	def init_hidden(self, batch_size):
 		return (autograd.Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size)),
@@ -27,4 +27,12 @@ class LookupTableManual(nn.Module):
 		for i in range(18):
 			es.append(self.embedding(input2.narrow(1, i, 3)))
 		embeddings2 = torch.stack(es, 1)
-		return None
+		
+		ts = embeddings1.view(batch_size, 5, -1)
+		ps = embeddings2.view(batch_size, 18, -1).transpose(1, 2)
+		
+		rs = torch.bmm(ts, ps)
+		ms = torch.max(rs, dim = 2)[0]
+		ms = torch.max(ms, dim = 1)[0]
+		logits = F.log_softmax(self.linear(ms))
+		return logits
