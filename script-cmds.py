8c720194e211b26779e5e0ad02a99aa0a6189703
German Kruszewski <germank@fb.com>
2018-07-14 15:29:14 +0200

    results auto pos

--- script-cmds.py
+++ script-cmds.py
@@ -2,7 +2,7 @@ k=5
 for s in range(100):
     for e in [5, 10, 20]:
         for l in [0.01, 0.001]:
-            for m in ["auto", "auto_pos"]:
+            for m in ["auto_pos"]:
                 CMD = "python script1.py -s {} -e {} -l {} -m {} -k {} -o {}".format(
                         s,
                         e,
